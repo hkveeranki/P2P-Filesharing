6ec1310e9e8db3692c69cf6573e659ec7f5688a9
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2018-06-23 19:00:44 +0530

    Better Debugging
    - Use python's `logging` library to do the logging
    - Write debug outputs to stderr rather than stodut
    Signed-off-by: Hemanth Kumar Veeranki <hems.india1997@gmail.com>

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
 import socket
 import os
+import sys
 from datetime import datetime
 import hashlib
+import logging
+
+__author__ = 'harry7'
+LOG_FILE = 'client_log.log'
+LOG_LEVEL = logging.DEBUG
+addr = ''
 
 
-def close_server():
-    timel = datetime.now().strftime("%I:%M%p %B %d, %Y")
-    log.write("------- Connection Closed at " + timel + " -------\n")
-    log.close()
+def close_client():
+    time_end = datetime.now().strftime('%I:%M%p %B %d, %Y')
+    logging.info('------- Connection Closed at ' + time_end + ' -------')
     s.close()
     exit(0)
 
@@ -15,123 +21,133 @@ def close_server():
 def file_download(args, filename, flag):
     s.send(args)
     data = s.recv(1024)
-    if flag != "UDP" and flag != "TCP":
-        print "Wrong Arguments"
-        print "Format FileDownload <TCP/UDP> <file_name>"
+
+    def log_error(exception):
+        logging.error('FileDownload for %s failed: %s' % (filename, exception))
+
+    if flag != 'UDP' and flag != 'TCP':
+        sys.stderr.write('Wrong Arguments\n')
+        sys.stderr.write('Format FileDownload <TCP/UDP> <file_name>\n')
         return
-    if data != "recieved":
+    if data != 'received':
         print data
         return
-    if flag == "UDP":
-        nport = int(s.recv(1024))
-        ncs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        addr = (host, nport)
-        ncs.sendto("recieved", addr)
+    if flag == 'UDP':
+        port_received = int(s.recv(1024))
+        new_con_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        addr = (host, port_received)
+        new_con_soc.sendto('received', addr)
         try:
-            f = open(filename, "wb+")
-        except:
-            print "Insufficient Privileges or Space"
+            f = open(filename, 'wb+')
+        except Exception as e:
+            sys.stderr.write('Insufficient Privileges or Space\n')
+            log_error(e)
             return
         while True:
-            data, addr = ncs.recvfrom(1024)
-            if data == "done":
+            data, addr = new_con_soc.recvfrom(1024)
+            if data == 'done':
                 break
             f.write(data)
-            ncs.sendto("recieved", addr)
+            new_con_soc.sendto('received', addr)
         f.close()
-        ncs.close()
-    elif flag == "TCP":
+        new_con_soc.close()
+    elif flag == 'TCP':
         try:
-            f = open(filename, "wb+")
-        except:
-            print "Insufficient Privileges or Space"
+            f = open(filename, 'wb+')
+        except Exception as e:
+            sys.stderr.write('Insufficient Privileges or Space\n')
+            log_error(e)
             return
         while True:
             data = s.recv(1024)
-            if data == "done":
+            if data == 'done':
                 break
             f.write(data)
-            s.send("recieved")
+            s.send('received')
         f.close()
     hash1 = s.recv(1024)
     f = open(filename, 'rb')
     orig_hash = hashlib.md5(f.read()).hexdigest()
     if hash1 != orig_hash:
         # print hash,orig_hash
-        print "File Sent Failed"
+        sys.stderr.write('File download failed')
+        logging.warning('FileDownload failed for %s Hash mismatch' % filename)
     else:
-        s.send("sendme")
+        s.send('sendme')
         data = s.recv(1024)
-        print
         print data
-        print "md5hash: ", hash1
-        print "Successfulluy Downloaded"
+        logging.debug('FileDownload for %s successful' % filename)
 
 
 def recieve_data(inp):
+    def log_error(exception):
+        sys.stderr.write('Error in Connection\n')
+        logging.error('Could not send data to server %s' % exception)
+        close_client()
+
     try:
         s.send(inp)
-    except:
-        print "Error in Connection"
-        return
+    except Exception as e:
+        log_error(e)
     while True:
         try:
             data = s.recv(1024)
-        except:
-            print "Error in Connection"
-            close_server()
-            break
-        if data == "done":
-            break
+            if data == 'done':
+                break
+            print data
+        except Exception as e:
+            log_error(e)
         try:
-            s.send("recieved")
-        except:
-            print "Connection Error"
-            close_server()
-        print data
-    return
+            s.send('received')
+        except Exception as e:
+            log_error(e)
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = raw_input("Host ip: ")
-port = input("PORT: ")
-down = raw_input("Download Folder: ")
-if not os.path.exists(down):
-    print "No Such Folder"
-    exit(0)
-elif not os.access(down, os.W_OK):
-    print "No Privilleges"
-    exit(0)
-else:
-    os.chdir(down)
-try:
-    log = open("client_log.log", "a+")
-except:
-    print "Cannot Open Log file"
-    exit(0)
-try:
-    s.connect((host, port))
-except:
-    print "No available server found on given address"
-    s.close()
-    exit(0)
-cnt = 0
-print "Connection Established"
-time = datetime.now().strftime("%I:%M%p %B %d, %Y")
-log.write("------- Connected to " + host + " at " + time + " -------\nCommands Sent:\n")
-while True:
-    cnt += 1
-    args = raw_input("Enter Command: ")
-    inp = args.split()
-    log.write(str(cnt) + ". " + args + "\n")
-    if len(inp) == 0 or inp[0] == "close":
-        s.send(args)
-        print "Bye"
-        close_server()
-    elif inp[0] == "IndexGet" or inp[0] == "FileHash":
-        recieve_data(args)
-    elif inp[0] == "FileDownload":
-        file_download(args, " ".join(inp[2:]), inp[1])
+if __name__ == '__main__':
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = raw_input('Host ip: ')
+    port = input('PORT: ')
+    down = raw_input('Download Folder: ')
+
+    if not os.path.exists(down):
+        sys.stderr.write('Download folder does not exist')
+        exit(-1)
+    elif not os.access(down, os.W_OK):
+        sys.stderr.write('Insufficient privileges on download folder\n')
+        exit(-1)
     else:
-        print "Invalid Command"
-s.close()
+        os.chdir(down)
+
+    try:
+        s.connect((host, port))
+    except:
+        sys.stderr.write('No available server found on given address\n')
+        s.close()
+        exit(-1)
+    cnt = 0
+
+    try:
+        logging.basicConfig(filename=LOG_FILE, level=LOG_LEVEL)
+        logging.debug('Starting the Client')
+    except Exception as e:
+        sys.stderr.write('Logging error %s\n' % e)
+        exit(-1)
+
+    time = datetime.now().strftime('%I:%M%p %B %d, %Y')
+    logging.info('------- Connected to ' + host + ' at ' + time + ' -------')
+    logging.debug('Commands sent:')
+    while True:
+        cnt += 1
+        args = raw_input('Enter command: ')
+        inp = args.split()
+        logging.debug(str(cnt) + '. ' + args + '')
+        if len(inp) == 0 or inp[0] == 'close':
+            s.send(args)
+            close_client()
+        elif inp[0] == 'IndexGet' or inp[0] == 'FileHash':
+            recieve_data(args)
+        elif inp[0] == 'FileDownload':
+            file_download(args, ' '.join(inp[2:]), inp[1])
+        else:
+            sys.stderr.write('Invalid Command %s\n' % inp)
--- server.py
+++ server.py
@@ -2,7 +2,13 @@ import socket
 import os
 import random
 import re
+import logging
+import sys
 from datetime import datetime
+
+__author__ = 'harry7'
+LOG_FILE = 'server_log.log'
+LOG_LEVEL = logging.DEBUG
 
 
 def create_port(socket):
@@ -14,264 +20,260 @@ def create_port(socket):
     return np
 
 
+def log_error(exception):
+    global addr
+    logging.error('Connection Error to %s\n %s' % (addr, exception))
+
+
 def regex(s, inp):
     reg = inp[1]
     fl = False
     files = os.popen("find . -not -path '*/\.*' -type f").read().split('\n')
     if len(files) == 1:
-        s.send("No Files Found")
+        s.send('No Files Found')
         return
     try:
         for j in files:
-            if j != "" and re.search(reg, j) != None and re.search(reg, j).group(0) != '':
-                j = '"' + j + '"'
-                cmd = "stat --printf 'name: %n \tSize: %s bytes\t Type: %F\t Timestamp:%z\n' " + j
+            if j != '' and re.search(reg, j) != None and re.search(reg, j).group(0) != '':
+                j = ''' + j + '''
+                cmd = "stat --printf 'name: %n \tSize: %s bytes\t Type: %F\t Timestamp:%z' " + j
                 res = os.popen(cmd).read()
                 s.send(res)
                 fl = True
-                if s.recv(1024) != "recieved":
+                if s.recv(1024) != 'received':
                     break
         if not fl:
-            s.send("No Files Found")
-        s.send(" ")
+            s.send('No Files Found')
+        s.send(' ')
         s.recv(1024)
-        s.send("done")
-    except:
-        print "Bad Connection Error"
-        return
+        s.send('done')
+    except Exception as e:
+        log_error(e)
 
 
 def longlist(s):
     files = os.popen("find . -not -path '*/\.*' -type f").read().split('\n')
     if len(files) == 1:
-        s.send("No Files Found")
+        s.send('No Files Found')
         return
     try:
         for j in files:
-            if j != "":
-                j = '"' + j + '"'
-                cmd = "stat --printf 'name: %n \tSize: %s bytes\t Type: %F\t Timestamp:%z\n' " + j
+            if j != '':
+                j = '\'' + j + '\''
+                cmd = "stat --printf 'name: %n \tSize: %s bytes\t Type: %F\t Timestamp:%z' " + j
                 res = os.popen(cmd).read()
                 s.send(res)
-                if s.recv(1024) != "recieved":
+                if s.recv(1024) != 'received':
                     break
-        s.send(" ")
+        s.send(' ')
         s.recv(1024)
-        s.send("done")
-    except:
-        print "Bad Connection Error"
-        return
+        s.send('done')
+    except Exception as e:
+        log_error(e)
 
 
 def shortlist(s, inp):
     inp = inp.split()
-    time1 = inp[2] + " " + inp[3]
-    time2 = inp[4] + " " + inp[5]
+    time1 = inp[2] + ' ' + inp[3]
+    time2 = inp[4] + ' ' + inp[5]
     files = os.popen("find %s -newermt %s ! -newermt  %s -not -path '*/\.*' -type f" % (
-        ".", str('"' + time1 + '"'), str('"' + time2 + '"'))).read().split('\n')
+        '.', str('\'' + time1 + '\''), str('\'' + time2 + '\''))).read().split('\n')
     if len(files) == 1:
-        s.send("No Files Found")
+        s.send('No Files Found')
         s.recv(1024)
         return
     try:
         for j in files:
-            if j != "":
-                j = '"' + j + '"'
-                cmd = "stat --printf 'name: %n \tSize: %s bytes\t Type: %F\t Timestamp:%z\n' " + j
+            if j != '':
+                j = '\'' + j + '\''
+                cmd = "stat --printf 'name: %n \tSize: %s bytes\t Type: %F\t Timestamp:%z'" + j
                 res = os.popen(cmd).read()
                 s.send(res)
-                if s.recv(1024) != "recieved":
+                if s.recv(1024) != 'received':
                     break
-        s.send(" ")
+        s.send(' ')
         s.recv(1024)
-        s.send("done")
-    except:
-        print "Bad Connection Error"
-        return
+        s.send('done')
+    except Exception as e:
+        log_error(e)
 
 
 def verify(s, filenam, fl=True):
-    filename = '"' + filenam + '"'
-    cmd = "stat --printf '%z\n' " + filename
-    t = os.popen(cmd).read().split('\n')[0]
-    if t == "":
-        s.send("No Such File")
-
-    else:
-        try:
-            cmd = "cksum " + filename
-            h = os.popen(cmd).read().split('\n')[0].split()[0]
-            h = "checksum: " + h + "\n"
-            t = "last modified: " + t
-            str = "file: " + filenam
-            res = [str, t, h]
-            for i in res:
-                s.send(i)
-                if s.recv(1024) != "recieved":
-                    break
-        except:
-            print "Bad Connection Error"
-            return
+    filename = '\'' + filenam + '\''
+    cmd = "stat --printf '%z' " + filename
+    t = os.popen(cmd).read().split('')[0]
+    if t == '':
+        s.send('No Such File')
+        return
+    try:
+        cmd = 'cksum ' + filename
+        h = os.popen(cmd).read().split('')[0].split()[0]
+        h = 'checksum: ' + h + ''
+        t = 'last modified: ' + t
+        str = 'file: ' + filenam
+        res = [str, t, h]
+        for i in res:
+            s.send(i)
+            if s.recv(1024) != 'received':
+                break
+    except Exception as e:
+        log_error(e)
 
 
 def file_send(s, args):
     inp = args.split()
     flag = inp[1]
-    filename = " ".join(inp[2:])
-    err = os.popen('ls "' + filename + '"').read().split('\n')[0]
-    if err == "":
-        s.send("No Such File or Directory")
+    filename = ' '.join(inp[2:])
+    err = os.popen('ls \'' + filename + '\'').read().split('\n')[0]
+    if err == '':
+        s.send('No Such File or Directory')
         return
-    s.send("recieved")
-    if flag == "UDP":
+    s.send('received')
+    if flag == 'UDP':
         ncs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         nport = create_port(ncs)
         s.send(str(nport))
         data, addr = ncs.recvfrom(1024)
-        if data == "recieved":
+        if data == 'received':
             try:
-                f = open(filename, "rb")
+                f = open(filename, 'rb')
                 byte = f.read(1024)
                 while byte:
                     ncs.sendto(byte, addr)
                     data, addr = ncs.recvfrom(1024)
-                    if data != "recieved":
+                    if data != 'received':
                         break
                     byte = f.read(1024)
-                ncs.sendto("done", addr)
-            except:
-                print "Bad Connection Error"
+                ncs.sendto('done', addr)
+            except Exception as e:
+                log_error(e)
                 return
 
-    elif flag == "TCP":
+    elif flag == 'TCP':
         try:
-            f = open(filename, "rb")
+            f = open(filename, 'rb')
             byte = f.read(1024)
             while byte:
                 s.send(byte)
-                if s.recv(1024) != "recieved":
+                if s.recv(1024) != 'received':
                     break
                 byte = f.read(1024)
-            s.send("done")
-        except:
-            print "Bad Connection Error"
+            s.send('done')
+        except Exception as e:
+            log_error(e)
             return
     else:
-        print "Wrong Arguments"
+        logging.info('Bad Arguments provided')
         return
-    hash = os.popen('md5sum "' + filename + '"').read().split()[0]
+    hash = os.popen('md5sum \'' + filename + '\'').read().split()[0]
     s.send(hash)
-    cmd = "stat --printf 'name: %n \tSize: %s bytes\t Timestamp:%z\n' " + filename
+    cmd = "stat --printf 'name: %n \tSize: %s bytes\t Timestamp:%z' " + filename
     res = os.popen(cmd).read()
     if s.recv(1024) == 'sendme':
         s.send(res)
-        print "Done"
 
 
 def checkall(s):
-    files = os.popen("find . -not -path '*/\.*' -type f").read().split('\n')
+    files = os.popen("find . -not -path '*/\.*' -type f").read().split('')
     for i in files:
-        if i != "":
+        if i != '':
             verify(s, i, False)
-    s.send("done")
+    s.send('done')
 
 
 # Main Code
-
-port = input("PORT: ")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "0.0.0.0"
-try:
-    s.bind((host, port))
-except:
-    print "Socket creation Error"
-    exit(0)
-s.listen(5)
-shared = raw_input("FullPath of Shared Folder: ")
-
-try:
-    log = open("server_log.log", "a+")
-except:
-    print "Cannot Open Log file"
-    exit(0)
-
-if not os.path.exists(shared):
-    print "No Such Folder"
-    exit(0)
-elif not os.access(shared, os.R_OK):
-    print "No Privilleges"
-    exit(0)
-else:
-    os.chdir(shared)
-
-print "Directory changed"
-print "Server is Up and Listening"
-times = datetime.now().strftime("%I:%M%p %B %d, %Y")
-log.write("********* Server Started at " + times + " *********\n\n")
-while True:
+if __name__ == '__main__':
+    port = input('PORT: ')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '0.0.0.0'
     try:
-        cs, addr = s.accept()
-    except:
-        s.close()
-        print
-        print "Bye"
-        timel = datetime.now().strftime("%I:%M%p %B %d, %Y")
-        log.write("\n********* Server Closed at " + timel + " *********\n\n")
-        log.close()
-        exit(0)
-    cnt = 0
-    time = datetime.now().strftime("%I:%M%p %B %d, %Y")
-    print("Got a connection from %s" % str(addr))
-    log.write("------- Got a connection from " + str(addr) + "at " + time + " -------\n Commands Executed:\n")
+        s.bind((host, port))
+    except Exception as e:
+        sys.stderr.write('Socket creation Error %s\n' % e)
+        exit(-1)
+    s.listen(5)
+    shared = raw_input('FullPath of Shared Folder: ')
+    if not os.path.exists(shared):
+        sys.stderr.write('Shared folder doesn\'t exist\n')
+        exit(-1)
+    elif not os.access(shared, os.R_OK):
+        sys.stderr.write('No Privilleges on the shared directory\n')
+        exit(-1)
+    else:
+        os.chdir(shared)
+    try:
+        logging.basicConfig(filename=LOG_FILE, level=LOG_LEVEL)
+        logging.debug('Starting the server')
+    except Exception as e:
+        sys.stderr.write('Logging error %s\n' % e)
+        exit(-1)
+    sys.stderr.write('Server is Up and Running')
+    time_start = datetime.now().strftime('%I:%M%p %B %d, %Y')
+    logging.info('********* Server Started at ' + time_start + ' *********')
+
     while True:
-        cnt += 1
         try:
-            args = cs.recv(1024)
-            log.write(str(cnt) + ". " + args + "\n")
-        except:
-            print "Connection closed to client"
-            timel = datetime.now().strftime("%I:%M%p %B %d, %Y")
-            log.write("------- Connection Closed at " + timel + " -------\n")
-            break
-        p = args.split()
-        if len(p) == 0 or p[0] == "close":
-            cs.close()
-            print "Connection closed to client"
-            timel = datetime.now().strftime("%I:%M%p %B %d, %Y")
-            log.write("------- Connection Closed at " + timel + " -------\n")
-            break
-        elif p[0] == "IndexGet":
-            if p[1] == "longlist":
-                # long list
-                longlist(cs)
-            elif len(p) == 2:
-                # regex
-                regex(cs, p)
-            elif len(p) == 6:
-                shortlist(cs, args)
-            elif p[1] == "shortlist":
-                try:
-                    cs.send("Syntax error")
-                    cs.send("Input Format IndexGet shotlist date1 time1 date2 time2")
-                    cs.send("done")
-                except:
-                    print "Connection Error"
-                    break
-        elif p[0] == "FileHash":
-            if p[1] == "verify":
-                verify(cs, p[2])
-            elif p[1] == "checkall" and len(p) == 2:
-                checkall(cs)
+            global addr
+            cs, addr = s.accept()
+        except Exception as e:
+            s.close()
+            sys.stderr.write('Shutting Down the server')
+            logging.info('\n********* Server Closed at ' +
+                         datetime.now().strftime('%I:%M%p %B %d, %Y') +
+                         ' *********\n')
+            exit(0)
+        cnt = 0
+        time = datetime.now().strftime('%I:%M%p %B %d, %Y')
+        logging.debug('------- Got a connection from ' + str(addr) + 'at ' + time +
+                      ' -------\n')
+        logging.debug('Commands Executed:')
+        while True:
+            cnt += 1
+            try:
+                args = cs.recv(1024)
+                logging.debug(str(cnt) + '. ' + args + '')
+            except Exception as e:
+                time_c_end = datetime.now().strftime('%I:%M%p %B %d, %Y')
+                logging.debug('------- Connection Closed at ' + time_c_end + ' -------')
+                break
+            p = args.split()
+            if len(p) == 0 or p[0] == 'close':
+                cs.close()
+                time_c_end = datetime.now().strftime('%I:%M%p %B %d, %Y')
+                logging.debug('------- Connection Closed at ' + time_c_end + ' -------')
+                break
+            elif p[0] == 'IndexGet':
+                if p[1] == 'longlist':
+                    # long list
+                    longlist(cs)
+                elif len(p) == 2:
+                    # regex
+                    regex(cs, p)
+                elif len(p) == 6:
+                    shortlist(cs, args)
+                elif p[1] == 'shortlist':
+                    try:
+                        cs.send('Syntax error')
+                        cs.send('Input Format IndexGet shotlist date1 time1 date2 time2')
+                        cs.send('done')
+                    except Exception as e:
+                        logging.error('Connection error to %s' % str(addr))
+                        break
+            elif p[0] == 'FileHash':
+                if p[1] == 'verify':
+                    verify(cs, p[2])
+                elif p[1] == 'checkall' and len(p) == 2:
+                    checkall(cs)
+                else:
+                    try:
+                        cs.send('Invalid Arguments')
+                        cs.send('done')
+                    except Exception as e:
+                        logging.error('Connection Error while sending data to %s\n' % str(addr))
+                        break
+            elif p[0] == 'FileDownload':
+                file_send(cs, args)
             else:
-                try:
-                    cs.send("Invalid Arguments")
-                    cs.send("done")
-                except:
-                    print "Connection Error"
-                    break
-        elif p[0] == "FileDownload":
-            file_send(cs, args)
-        else:
-            cs.send("Invalid Command")
-            cs.send("done")
-    cs.close()
+                cs.send('Invalid Command')
+                cs.send('done')
+        cs.close()
