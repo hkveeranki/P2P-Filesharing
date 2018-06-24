fdf2768e1a8f3340375492b2499e0dad819f752a
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2018-06-24 20:24:54 +0530

    Organise code into module
    Signed-off-by: Hemanth Kumar Veeranki <hems.india1997@gmail.com>

---
+++ p2pfileshare/__init__.py
@@ -0,0 +1,7 @@
+"""
+p2pfileshare module.
+Provides a cli based p2p filesharing system which consists of a server and client. Works well for linux systems
+"""
+
+__author__ = 'harry7'
+__version = '0.1.0'
